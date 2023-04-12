44c25e71ae10c926aa729183f0f17db15e06f304
dermie83 <dermiemadsen@gmail.com>
2023-04-12 18:27:04 +0100

    YAML file initial setup

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -12,26 +12,28 @@ public class Dashboard extends Controller
   public static void index() {
     Logger.info("Rendering Dashboard");
 
-    Reading r1 = new Reading(400,5.6,3.3,1001);
-    Reading r2 = new Reading(500,6.0,5.3,990);
-    Reading r3 = new Reading(700,7.3,4.3,1100);
-    Station s1 = new Station("Tramore");
-    s1.readings.add (r1);
-    s1.readings.add (r2);
-    s1.readings.add (r3);
+//    Reading r1 = new Reading(400,5.6,3.3,1001);
+//    Reading r2 = new Reading(500,6.0,5.3,990);
+//    Reading r3 = new Reading(700,7.3,4.3,1100);
+//    Station s1 = new Station("Tramore");
+//    s1.readings.add (r1);
+//    s1.readings.add (r2);
+//    s1.readings.add (r3);
+//
+//    Reading r4 = new Reading(500,6.6,6.3,900);
+//    Reading r5 = new Reading(700,7.1,5.6,1000);
+//    Reading r6 = new Reading(900,3.3,6.3,1100);
+//    Station s2 = new Station("Derry");
+//    s2.readings.add (r4);
+//    s2.readings.add (r5);
+//    s2.readings.add (r6);
+//
+//    List<Station> stations = new ArrayList<Station>();
+//    stations.add (s1);
+//    stations.add (s2);
 
-    Reading r4 = new Reading(500,6.6,6.3,900);
-    Reading r5 = new Reading(700,7.1,5.6,1000);
-    Reading r6 = new Reading(900,3.3,6.3,1100);
-    Station s2 = new Station("Derry");
-    s2.readings.add (r4);
-    s2.readings.add (r5);
-    s2.readings.add (r6);
 
-    List<Station> stations = new ArrayList<Station>();
-    stations.add (s1);
-    stations.add (s2);
-
+    List<Station> stations = Station.findAll();
     render ("dashboard.html", stations);
   }
 }
