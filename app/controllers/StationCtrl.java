4a73c689ba1316252e9b979788f7a4cef90d124a
dermie83 <dermiemadsen@gmail.com>
2023-04-24 21:48:08 +0100

    Add a delete button

--- app/controllers/StationCtrl.java
+++ app/controllers/StationCtrl.java
@@ -6,12 +6,23 @@ import models.Station;
 import models.Reading;
 import play.mvc.Controller;
 
-public class StationCtrl extends Controller
-{
-    public static void index(Long id)
+public class StationCtrl extends Controller {
+    public static void index(Long id) {
+        Station station = Station.findById(id);
+        Logger.info("Station id = " + id);
+        render("station.html", station);
+    }
+
+    public static void deleteReading(Long id, Long readingID)
     {
         Station station = Station.findById(id);
-        Logger.info ("Station id = " + id);
+        Reading reading = Reading.findById(readingID);
+        station.readings.remove(reading);
+        station.save();
+        reading.delete();
+        Logger.info ("Removing " + reading.code);
+
         render("station.html", station);
+
     }
 }
